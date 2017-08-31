8c9e462216dea83489327242980aa72d1d23cdbd
siddharthvartak <siddharth.vartak@playphone.com>
2017-08-30 18:51:57 -0700

    changing ip address

--- src/main/java/com/playphone/nativeAndroid/ShowPopUp.java
+++ src/main/java/com/playphone/nativeAndroid/ShowPopUp.java
@@ -11,12 +11,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.annotations.*;
 
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 
-public class Clickinit {
+public class ShowPopUp {
 WebDriver driver;
 	
 	
@@ -29,19 +28,12 @@ WebDriver driver;
 	  caps.setCapability(MobileCapabilityType.DEVICE_NAME, "56c34fc8");
 	  caps.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
 	  caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "100");
-	  driver = new RemoteWebDriver(new URL("http://0.0.0.0:4723/wd/hub"),caps);
+	  driver = new RemoteWebDriver(new URL("http://172.16.1.180:4723/wd/hub"),caps);
 	}
 	@Test
 	public void clickInit() throws InterruptedException{
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.id("com.playphone.msports.sdk.test:id/init")).click();
+		driver.findElement(By.id("com.playphone.msports.sdk.test:id/popup")).click();
 
 }
-
-@AfterClass(alwaysRun = true)
-public void tearDown() throws MalformedURLException {
-	  //Thread.sleep(4000);
-  driver.quit();
-  
-  }
 }
